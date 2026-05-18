eea5c16d05da26edec8592b8969f1394eaf3d05d
dpwebb <davidphilipwebb@gmail.com>
2026-05-17 21:43:07 -0300

    Register outcome admin review route

--- server.ts
+++ server.ts
@@ -183,6 +183,20 @@ app.get('_api/outcomes/get',async c => {
     return c.text("Error loading endpoint code " + e.message,  500)
   }
 })
+app.post('_api/outcomes/admin-review',async c => {
+  try {
+    const { handle } = await import("./endpoints/outcomes/admin-review_POST.js");
+    let request = c.req.raw;
+    const response = await handle(request);
+    if (!(response instanceof Response) && response.constructor.name !== "Response") {
+      return c.text("Invalid response format. handle should always return a Response object." + response.constructor.name, 500);
+    }
+    return response;
+  } catch (e) {
+    console.error(e);
+    return c.text("Error loading endpoint code " + e.message,  500)
+  }
+})
 app.post('_api/pdf/report',async c => {
   try {
     const { handle } = await import("./endpoints/pdf/report_POST.js");
